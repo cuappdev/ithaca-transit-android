7f9516202c815018600abac5fd438c4addbfb54e
Ziwei Gu <zg48@cornell.edu>
2018-10-31 18:14:42 -0400

    # This is a combination of 4 commits. # This is the 1st commit message:
    # This is a combination of 8 commits.
    # This is the 1st commit message:
    
    Initialized Google Map
    
    # This is the commit message #2:
    
    beginning to implement models
    
    # This is the commit message #3:
    
    fixed superclass issue in Direction
    
    # This is the commit message #4:
    
    finished basis of Direction model
    
    # This is the commit message #5:
    
    edited BusPath model
    
    # This is the commit message #6:
    
    combining Place, PlaceResult models
    
    removed semicolons
    
    # This is the commit message #7:
    
    Fixing places/maps integration
    
    # This is the commit message #8:
    
    Adressing PR reviews
    
    # This is the commit message #2:
    
    Addresses remainder of issue from pr review
    
    # This is the commit message #3:
    
    Design views for routes
    
    # This is the commit message #4:
    
    Add fetch and move to current location

--- app/src/main/java/ithaca_transit/android/cornellappdev/com/ithaca_transit/MainActivity.kt
+++ app/src/main/java/ithaca_transit/android/cornellappdev/com/ithaca_transit/MainActivity.kt
@@ -1,34 +1,82 @@
 package ithaca_transit.android.cornellappdev.com.ithaca_transit
 
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
+    private fun setMapLongClick(map: GoogleMap) {
+        map.setOnMapLongClickListener { latLng ->
+            val snippet = String.format(Locale.getDefault(),
+                    "Lat: %1$.5f, Long: %2$.5f",
+                    latLng.latitude,
+                    latLng.longitude)
+
+            map.addMarker(MarkerOptions()
+                    .position(latLng)
+                    .title(getString(R.string.app_name))
+                    .snippet(snippet))
         }
-        false
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        setMapLongClick(mMap)
+        setUpMap()
+    }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+        mMap.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 14f))
+            }
+        }
     }
 }
